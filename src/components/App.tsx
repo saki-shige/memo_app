3e185ff4d516857c633b3ed45e5fa75f12cf7fdb
重田早紀 <shigetasaki@shigetasakinoMacBook-Pro.local>
2022-07-14 19:23:40 +0900

    [fix]コンポーネント化

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
-import { ChangeEvent, FC, useState } from "react";
+import { ChangeEvent, FC, useState, useCallback, memo } from "react";
 import styled from 'styled-components';
+import { MemoList } from "./MemoList"
 
-export const App:FC = () => {
+// FC function component
+export const App:FC = memo(() => {
+    console.log("app");
     const [text, setText] = useState<string>("");
     const [memos, setMemos] = useState<string[]>([]);
+    // 引数の型　ChangeEvent<HTMLInputElement>
     const onChangeText = (e: ChangeEvent<HTMLInputElement>) => setText(e.target.value);
 
     const onClickAdd = () => {
+        console.log("clickadd")
         const newMemos = [...memos];
         newMemos.push(text);
         memos.push(text);
@@ -14,35 +19,23 @@ export const App:FC = () => {
         setText("");
     };
 
-    const onClickDelete = (index: number) => {
+    const onClickDelete = useCallback((index: number) => {
+        console.log("clickdelete")
         const newMemos = [...memos];
         newMemos.splice(index,1);
         setMemos(newMemos);
-    };
+    }, [memos]);
 
     return(
         <div>
           <h1>簡単メモアプリ</h1>
           <input type="text" value={text} onChange={onChangeText}/ >
           <SButton onClick={onClickAdd}>追加</SButton>
-          <ul>
-             {memos.map((memo,i) => (
-                <li key={i}>
-                    <SMemoWrapper>
-                      <p>{memo}</p>
-                      <SButton onClick={() => onClickDelete(i)}>削除</SButton>
-                    </SMemoWrapper>
-                </li>
-             ))}
-          </ul>
+          <MemoList memos={memos} onClickDelete={onClickDelete} />
         </div>
     );
-};
+});
 
 const SButton = styled.button`
     margin-left: 16px;
-`;
-const SMemoWrapper = styled.div`
-    display: flex;
-    algin-items: center;
 `;
---
+++ src/components/MemoList.tsx
@@ -0,0 +1,37 @@
+import { FC, memo } from "react"
+import styled from "styled-components"
+
+// type 型定義を変数化
+type Props = {
+    memos: string[];
+    // void　関数が何も返却しない　return文を記述するとエラーすることができる
+    onClickDelete: (index: number ) => void
+};
+
+// (props: Props)
+export const MemoList: FC<Props> = memo((props) => {
+  const { memos, onClickDelete } = props;
+  console.log("memolist");
+
+
+  return(
+    <ul>
+    {memos.map((memo,i) => (
+       <li key={i}>
+           <SMemoWrapper>
+             <p>{memo}</p>
+             <SButton onClick={() => onClickDelete(i)}>削除</SButton>
+           </SMemoWrapper>
+       </li>
+    ))}
+    </ul>
+  );
+});
+
+const SButton = styled.button`
+    margin-left: 16px;
+`;
+const SMemoWrapper = styled.div`
+    display: flex;
+    algin-items: center;
+`;
